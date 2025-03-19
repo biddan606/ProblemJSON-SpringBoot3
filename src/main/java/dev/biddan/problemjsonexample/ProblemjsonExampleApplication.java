596827c324ab291b0babe83ace73cef38d34b320
biddan606 <biddan606@gmail.com>
2025-03-20 01:22:05 +0900

    스프링부트3.0 프로젝트 생성

---
+++ src/main/java/dev/biddan/problemjsonexample/ProblemjsonExampleApplication.java
@@ -0,0 +1,13 @@
+package dev.biddan.problemjsonexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProblemjsonExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProblemjsonExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/biddan/problemjsonexample/ProblemjsonExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.biddan.problemjsonexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProblemjsonExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
